919da91f2628c6e4d144b4fd478b260fa06a063a
Natalie S. <161007805+natalie-christine@users.noreply.github.com>
2024-09-02 11:07:39 +0200

    Add files via upload

---
+++ PandasTabs.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+df = pd.read_csv(r"C:\Users\JodaBook\Documents\Python\Kursmaterialien\data\astronauts.csv")
+
+#print(df.head())
+#print(len(df))
+# for name in df["Name"]:
+#     print(name)
+#     break
+
+person = df.iloc[0]
+#print(person)
+personData = df.iloc[1]
+#print(personData)
+#print(person["Space Walks"])
+
+search = df.iloc[4:8] #Berreich
+search = df.iloc[-5:] #die letzen 5 Zeilen
+#print(search)
+
+# for row in df.iterrows(): 
+#     print(row)
+#     print(type(row))
+
+# for pos, d in df.iterrows(): 
+#     print(pos)
+#     print(d)
+#     print(d["Status"])
+#     break
+
+
+df2 = df.sort_values("Name", ascending=False)
+
+for pos, d in df2.iterrows():
+    if d["Status"] == "Active" and d["Year"] > 1990:
+        print(d["Name"])
